34ae88c757b8c24709d2f4c756c469905210d3bc
Proyecto112020 <70165424+Proyecto112020@users.noreply.github.com>
2020-08-26 14:18:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Daily Post</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="logo jorge.png">
+    <link rel="stylesheet" href="diseño.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<!-- Compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Minified JS library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>    
+<title>Document</title>
+</head>
+<body>
+    <div class="banner">
+    <img src="logo jorge.png" class="logo">
+    <div class="botones">
+    <button class="boton">Quienes somos</button>
+    <button class="boton">contactenos</button>
+    <button class="boton">sugerencias</button>
+    <button class="boton">ayuda</button>
+    <button class="boton">registrarme</button>
+    <button class="boton">login</button>
+    </div>
+    </div>
+    <div id="myCarousel" class="carousel slide mar" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        
+        <div class="item active">
+        <center> <img src="img1.jpg" class="img"></center>
+        </div>
+        <div class="item">
+        <center> <img src="img2.jpg" class="img"></center>
+        </div>
+        <div class="item">
+        <center> <img src="img3.jpg" class="img"></center>
+        </div>
+        
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<div class="f">
+<div class="d"><h1>Daily Post®</h1></div>
+<h5>Yulieth Gomez</h5>
+<h5>Jorge Mario Escobar</h5>
+<h5>David Agudelo Tapias</h5>
+<h5>Cesar Luis Hoyos</h5>
+
+</div>
+
+</body>
+
+</html>
