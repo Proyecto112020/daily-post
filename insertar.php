70d75c9cd6731e47a372bdd47767e0b4c92136e3
Proyecto112020 <70165424+Proyecto112020@users.noreply.github.com>
2020-08-27 14:25:44 -0500

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -6,9 +6,10 @@ $foton=$_FILES['foto']['name'];
 $fotot=$_FILES['foto']['type'];
 $fotos=$_FILES['foto']['size'];
 $carpeta=$_SERVER['document_root'].'/img';
+echo $carpeta;
 move_uploaded_file($_FILES['foto']['tmp_name'],$carpeta.$foton);
 include("conexion.php");
 $sql="INSERT INTO usuarios(id_usuario,nombre_usuario,usuario,contraseña,foto,clase) VALUES (NULL,'$nombre','$usuario','$clave','$foton',1)";
 $q=mysqli_query($conexion,$sql);
-header("location:index.php");
+
 ?>
