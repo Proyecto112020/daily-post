eb1efa0be696d77324e3d5c48dd13f2c244588be
Proyecto112020 <70165424+Proyecto112020@users.noreply.github.com>
2020-08-27 13:45:59 -0500

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -2,7 +2,7 @@
 $nombre=$_POST['nombre'];
 $usuario=$_POST['usuario'];
 $clave=$_POST['clave'];
-$foto=addslashes(file_get_contents($_FILES['foto']['tmp_name']));
+$foto=$_FILES['foto']['tmp_name'];
 include("conexion.php");
 $sql="INSERT INTO usuarios(id_usuario,nombre_usuario,usuario,contraseña,foto,clase) VALUES (NULL,'$nombre','$usuario','$clave','$foto',1)";
 $q=mysqli_query($conexion,$sql);
